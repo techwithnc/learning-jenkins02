4aebecf8270fdcd2940d625e97c6a76da42061af
TechWithNC <techwithnc.techwithnc@gmail.com>
2023-06-20 15:11:51 +0630

    add docker-compose file

--- CI_CD/scripts.sh
+++ CI_CD/scripts.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-# export app_v=$1
-docker compose -p jp01 up -d
-# sudo docker image pull techwithnc/betterhrapp:$app_v
-# sudo docker service update --image techwithnc/betterhrapp:$app_v svc01
+export app_v=$1
+# docker compose -p jp01 up -d
+docker swarm init
+docker service create --name svc01 techwithnc/betterhrapp:1.0
+docker image pull techwithnc/betterhrapp:$app_v
+docker service update --image techwithnc/betterhrapp:$app_v svc01
